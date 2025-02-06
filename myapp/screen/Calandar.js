3b8f7755991d67be42e05e170b547b1a74c752a6
Wongsathon Neanthasart <wongsathon.ne@ku.th>
2025-02-06 21:07:27 +0700

    rename calendar

--- myapp/screen/Calandar.js
+++ myapp/screen/Calandar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, TextInput, Image, FlatList, StyleSheet } from "react-native";
 import { AntDesign } from '@expo/vector-icons'; 
+import AppStack from "../navigation/AppStack";
 
 const timeSlots = [
   "08:00 a.m.",
