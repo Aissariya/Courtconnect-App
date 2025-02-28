45ebb6318c56457b22b62e58414807bf18fb2b91
yanisa24414 <yanisa.sangj@ku.th>
2025-02-28 10:58:04 +0700

    update  my booking / booking history

--- myapp/screen/BookingHistory.js
+++ myapp/screen/BookingHistory.js
@@ -1,20 +1,147 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-
-export default function BookingHistory({ navigation }) {
+export default function BookingHistory() {
   return (
-      <View style={styles.content}>
-        <Text>Welcome to Booking History Screen</Text>
-      </View>
+    <View style={styles.container}>
+      <View style={styles.card}>
+        
+        <Image source={require("../assets/football.jpg")} style={styles.image} />
 
+        
+        <View style={styles.cardContent}>
+          <Text style={styles.title}>สนามบาสหนองงูเห่า</Text>
+          <Text style={styles.text}>Date: 12 Dec 2024</Text>
+          <Text style={styles.text}>Time: 16:00 - 18:00</Text>
+          <Text style={styles.price}>Price: 200 THB</Text>
+        </View>
+
+        
+        <View style={[styles.statusContainer, styles.completed]}>
+          <Text style={styles.statusText}>สำเร็จ</Text>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1, 
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F6F9",
+    paddingHorizontal: 16,
+    paddingTop: 5,
+  },
+  card: {
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 15,
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#E0E0E0",
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    position: "relative", 
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 6,
+    marginRight: 15,
+  },
+  cardContent: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 5,
+    color: "#333",
+  },
+  text: {
+    fontSize: 14,
+    color: "#666",
+    marginBottom: 2,
+  },
+  price: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#1E88E5",
+    marginTop: 5,
+    color: "#009900",
+  },
+  statusContainer: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+   
+  },
+  statusText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "white",
+    
+  },
+  completed: {
+    backgroundColor: "#009900",
   },
 });
+
+
+
+/*import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function MyBook() {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.messageContainer}>
+        <View style={styles.messageBox}>
+          <Text style={styles.messageText}>No booking history available.</Text>
+        </View>
+      </View>
+
+      
+      <View style={styles.content}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F3F3",
+    paddingHorizontal: 10,
+  },
+  messageContainer: {
+    alignItems: "center",
+    marginVertical: 10,
+    
+  },
+  messageBox: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    marginTop:10,
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  content: {
+    flex: 1,
+  },
+});*/
+
